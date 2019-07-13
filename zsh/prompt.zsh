e49093037ca7716d8efbd89bab8d15f74ef284fa
r-richmond <rrichmond.gh@gmail.com>
2019-07-12 20:18:40 -0700

    Prompt configurations

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,80 +1,25 @@
+# See .antigenrc for powerlevel9k config
 
-# prompt is now set in antigen/
+POWERLEVEL9K_DIR_HOME_SUBFOLDER_BACKGROUND='steelblue'
+POWERLEVEL9K_VCS_CLEAN_BACKGROUND='blue'
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+  dir  # current directory
+  vcs  # git status
+)
 
-# autoload colors && colors
-# # cheers, @ehrenmurdick
-# # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
-#
-# if (( $+commands[git] ))
-# then
-#   git="$commands[git]"
-# else
-#   git="/usr/bin/git"
-# fi
-#
-# git_branch() {
-#   echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-# }
-#
-# git_dirty() {
-#   if $(! $git status -s &> /dev/null)
-#   then
-#     echo ""
-#   else
-#     if [[ $($git status --porcelain) == "" ]]
-#     then
-#       echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-#     else
-#       echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-#     fi
-#   fi
-# }
-#
-# git_prompt_info () {
-#  ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# # echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
-#  echo "${ref#refs/heads/}"
-# }
-#
-# # This assumes that you always have an origin named `origin`, and that you only
-# # care about one specific origin. If this is not the case, you might want to use
-# # `$git cherry -v @{upstream}` instead.
-# need_push () {
-#   if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]
-#   then
-#     number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) 2>/dev/null | wc -l | bc)
-#
-#     if [[ $number == 0 ]]
-#     then
-#       echo " "
-#     else
-#       echo " with %{$fg_bold[magenta]%}$number unpushed%{$reset_color%}"
-#     fi
-#   fi
-# }
-#
-# directory_name() {
-#   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
-# }
-#
-# battery_status() {
-#   if test ! "$(uname)" = "Darwin"
-#   then
-#     exit 0
-#   fi
-#
-#   if [[ $(sysctl -n hw.model) == *"Book"* ]]
-#   then
-#     $DOTFILES/bin/battery-status
-#   fi
-# }
-#
-# export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
-# set_prompt () {
-#   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-# }
-#
-# precmd() {
-#   title "zsh" "%m" "%55<...<%~"
-#   set_prompt
-# }
+POWERLEVEL9K_DATE_FORMAT="%D"
+POWERLEVEL9K_DATE_BACKGROUND='deepskyblue3'
+POWERLEVEL9K_TIME_BACKGROUND='deepskyblue3'
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+  status                  # exit code of the last command
+  command_execution_time  # duration of the last command
+  background_jobs         # presence of background jobs
+  kubecontext             # The current context of your kubectl configuration.
+  virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+  context                 # user@host
+  date                    # current date
+  time_joined             # current time
+)
+
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_RPROMPT_ON_NEWLINE=false
