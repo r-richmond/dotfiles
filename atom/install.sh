9ae75361af42d34123fc3a825c07bdc8c3cc03a8
r-richmond <rrichmond.gh@gmail.com>
2019-07-14 18:33:10 -0700

    Fix new functions

--- atom/install.sh
+++ atom/install.sh
@@ -2,5 +2,5 @@
 
 # Install Atom Packages
 # http://evanhahn.com/atom-apm-install-list/
-source $HOME/.dotfiles/functions/atom-package
+source "$DOTFILES"/functions/atom-package
 atom-package install
