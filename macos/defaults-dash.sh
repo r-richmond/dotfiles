898be53b4b41d7a5ac28ff5a4430e4a8090dfc37
r-richmond <rrichmond.gh@gmail.com>
2019-07-14 20:03:59 -0700

    Additional bug fixes

--- macos/defaults-dash.sh
+++ macos/defaults-dash.sh
@@ -14,8 +14,9 @@ FILE="/Users/$USER/Dropbox/applications/dash/Dash.dashsync";
 {
   # the following is necessary due to dialog prompt if license is active
   sleep 2
+  osascript -e 'tell application "DASH" to activate'
   osascript -e 'tell application "System Events" to key code 36' #return
-  sleep 2
+  sleep 1
   osascript -e 'quit app "DASH"'
 } || {
   echo "failed to quit dash in macos/defaults-dash.sh"
