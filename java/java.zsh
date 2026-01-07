37d3dc6de91ada35912ccf4b3e7dc3709b90f194
r-richmond <rrichmond.gh@gmail.com>
2026-01-06 20:35:20 -0800

    🔧 Misc Updates (#14)
    • GH CLI tool
    • Ghosty tweaks
    • Git Tweaks
    • VScode extension updates

--- java/java.zsh
+++ java/java.zsh
@@ -6,3 +6,18 @@
 
 # # For the system Java wrappers to find this JDK, symlink it with
 # # sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
+
+if [ -f /usr/libexec/java_home ]; then
+  export JAVA_HOME=$(/usr/libexec/java_home -v 17)
+  export PATH=$JAVA_HOME/bin:$PATH
+else
+  # sudo ln -sfn /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+fi
+
+if [ -d ~/flink ]; then
+  export FLINK_HOME=~/flink/flink-2.0.0
+  export PATH=$FLINK_HOME/bin:$PATH
+else
+  # sudo ln -sfn /opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-17.jdk
+fi
