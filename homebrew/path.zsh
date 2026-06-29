27dc3d9c6d0bc49c6bc8b0a166b65e250e113a49
r-richmond <rrichmond.gh@gmail.com>
2026-06-29 16:37:35 -0700

    tweaks

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -8,3 +8,9 @@ export PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
 
 # this sets gsed as sed
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+
+# Homebrew will now attempt to upgrade casks with `auto_updates true`.
+# Disable this behaviour with `HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1`.
+# Some casks with `auto_updates true` or `version :latest` may still require `--greedy`,
+# `HOMEBREW_UPGRADE_GREEDY` or `HOMEBREW_UPGRADE_GREEDY_CASKS` to be upgraded.
+export HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1
